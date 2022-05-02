0c658e7cd1d210984cf648d3d9d5689c45d1c69a
Hector Roussille <hector.roussille@gmail.com>
2022-05-02 10:10:15 +0200

    Inmproved base definition of Block Endorser

--- src/Roles/BlockEndorser.py
+++ src/Roles/BlockEndorser.py
@@ -1,10 +1,31 @@
 from Role import Role, RoleType
+from Common import Block
 
 
 class BlockEndorser(Role):
 
     def __init__(self) -> None:
         super().__init__(RoleType.BLOCK_ENDORSER)
+        self._endorsementStrategy = None
 
-    def endorseBlock(self):
+    @property
+    def endorsementStrategy(self):
+        return self._endorsementStrategy
+
+    @endorsementStrategy.setter
+    def endorsementStrategy(self, newEndorsementStrategy):
+        self._endorsementStrategy = newEndorsementStrategy
+
+    @endorsementStrategy.deleter
+    def endorsementStrategy(self):
+        del self._endorsementStrategy
+
+    def endorseBlock(self, block: Block) -> bool:
+        """ Endorse a specific block
+
+            :param block: the block to endorse
+            :type block: Block
+            :returns: wether the block was endorsed or not
+            :rtype: bool
+        """
         raise NotImplementedError
