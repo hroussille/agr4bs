5da3cec10ebb8a0f3767e844a0d4d155381596cc
Hector Roussille <hector.roussille@gmail.com>
2022-05-03 11:10:20 +0200

    Boilerplate on Block generic abstractions - tx list - creator

--- src/Common/Block.py
+++ src/Common/Block.py
@@ -1,4 +1,10 @@
+from array import array
+
+from src.Common.Transaction import Transaction
+
+
 class Block(object):
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, creator, transactions) -> None:
+        self._transactions = transactions
+        self._from = creator
