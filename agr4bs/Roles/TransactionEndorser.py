21ad1b3bd4958e1d6900929db1899a054eb15fbf
Hector Roussille <hector.roussille@gmail.com>
2022-05-06 12:05:41 +0200

    Refactored TransactionEndorser

--- agr4bs/Roles/TransactionEndorser.py
+++ agr4bs/Roles/TransactionEndorser.py
@@ -1,5 +1,17 @@
+from ..Agent import Agent, StateChange
 from ..Role import Role, RoleType
 from ..Common import Transaction
+
+
+class TransactionEndorserStateChange(StateChange):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        def _transactionEndorsementStrategy():
+            return True
+
+        self.transactionEndorsementStrategy = _transactionEndorsementStrategy
 
 
 class TransactionEndorser(Role):
@@ -7,9 +19,16 @@ class TransactionEndorser(Role):
     def __init__(self) -> None:
         super().__init__(RoleType.TRANSACTION_ENDORSER)
 
-    def endoreTransaction(self, transaction: Transaction) -> bool:
+    @staticmethod
+    def stateChange() -> StateChange:
+        return TransactionEndorserStateChange()
+
+    @staticmethod
+    def endorseTransaction(agent: Agent, transaction: Transaction, *args, **kwargs) -> bool:
         """ Endorse a specific transaction
 
+            :param agent: the agent on which the behavior operates
+            :type agent: Agent
             :param transaction: the transaction to endorse
             :type transaction: Transaction
             :returns: wether the transaction was endorsed or not
