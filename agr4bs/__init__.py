cd08c6f15b3561d40e4c1595510e0a5ab5eb880f
Hector Roussille <hector.roussille@gmail.com>
2022-05-06 12:03:12 +0200

    Exported StateChange class and RoleType enum from main package

--- agr4bs/__init__.py
+++ agr4bs/__init__.py
@@ -1,6 +1,8 @@
 from .Agent import Agent as Agent
+from .Agent import StateChange as StateChange
 from .Group import Group as Group
 from .Role import Role as Role
+from .Role import RoleType as RoleType
 from .Common import Transaction as Transaction
 from .Common import Payload as Payload
 from .Common import Block as Block
