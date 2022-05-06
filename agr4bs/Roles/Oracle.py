5f83cb6a78231c24df6b3ce5979301f5901439e8
Hector Roussille <hector.roussille@gmail.com>
2022-05-06 12:05:07 +0200

    Refactored Oracle

--- agr4bs/Roles/Oracle.py
+++ agr4bs/Roles/Oracle.py
@@ -1,7 +1,18 @@
+from agr4bs.Agent import StateChange
 from ..Role import Role, RoleType
+
+
+class OracleStateChange(StateChange):
+
+    def __init__(self) -> None:
+        super().__init__()
 
 
 class Oracle(Role):
 
     def __init__(self) -> None:
         super().__init__(RoleType.ORACLE)
+
+    @staticmethod
+    def stateChange() -> StateChange:
+        return OracleStateChange()
