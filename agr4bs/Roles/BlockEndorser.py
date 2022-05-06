b80f5715e0bb137ad6c83f7204192d0b4c0aac18
Hector Roussille <hector.roussille@gmail.com>
2022-05-06 12:04:03 +0200

    Refactored BlockEndorser

--- agr4bs/Roles/BlockEndorser.py
+++ agr4bs/Roles/BlockEndorser.py
@@ -1,28 +1,34 @@
+from ..Agent import Agent, StateChange
 from ..Role import Role, RoleType
 from ..Common import Block
+
+
+class BlockEndorserStateChange(StateChange):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        def _blockEndorsementStrategy():
+            return True
+
+        self.blockEndorsementStrategy = _blockEndorsementStrategy
 
 
 class BlockEndorser(Role):
 
     def __init__(self) -> None:
         super().__init__(RoleType.BLOCK_ENDORSER)
-        self._endorsementStrategy = None
 
-    @property
-    def endorsementStrategy(self):
-        return self._endorsementStrategy
+    @staticmethod
+    def stateChange() -> StateChange:
+        return BlockEndorserStateChange()
 
-    @endorsementStrategy.setter
-    def endorsementStrategy(self, newEndorsementStrategy):
-        self._endorsementStrategy = newEndorsementStrategy
-
-    @endorsementStrategy.deleter
-    def endorsementStrategy(self):
-        del self._endorsementStrategy
-
-    def endorseBlock(self, block: Block, *args, **kwargs) -> bool:
+    @staticmethod
+    def endorseBlock(agent: Agent, block: Block, *args, **kwargs) -> bool:
         """ Endorse a specific block
 
+            :param agent: the agent on which the behavior operates
+            :type agent: Agent
             :param block: the block to endorse
             :type block: Block
             :returns: wether the block was endorsed or not
