c02a042ac5c7090e5875043afea2b37452afeb27
Hector Roussille <hector.roussille@gmail.com>
2022-05-06 12:04:52 +0200

    Refactored Investee

--- agr4bs/Roles/Investee.py
+++ agr4bs/Roles/Investee.py
@@ -1,50 +1,58 @@
 from ..Role import Role, RoleType
 from ..Common import Investment
-from ..Agent import Agent
+from ..Agent import Agent, StateChange
+
+
+class InvesteeStateChange(StateChange):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.investors = {}
 
 
 class Investee(Role):
 
     def __init__(self) -> None:
         super().__init__(RoleType.INVESTEE)
-        self._investors = {}
 
-    def receive(self, agent: Agent, investment: Investment, *args, **kwargs):
+    @staticmethod
+    def stateChange() -> StateChange:
+        return InvesteeStateChange()
+
+    @staticmethod
+    def receiveInvestment(agent: Agent, investment: Investment, *args, **kwargs):
         """ Receive an investment from an Investor
 
-            :param agent: the agent to redistribute to
+            :param agent: the agent on which the behavior operates
             :type agent: Agent
             :param investment: the investment description
             :type investment: Investment
         """
         raise NotImplementedError
 
-    def redistribute(self, agent: Agent, *args, **kwargs) -> bool:
+    @staticmethod
+    def redistribute(agent: Agent,  investor: Agent, *args, **kwargs) -> bool:
         """ Redistribute earnings to a specific investor as investment
 
-            :param agent: the agent to redistribute to
+            :param agent: the agent on which the behavior operates
             :type agent: Agent
+            :param investor: the investor to redistribute to
+            :type investor: Agent
             :returns: wether redistribution was successfull or not
             :rtype: bool
         """
         raise NotImplementedError
 
-    def redistributeFull(self, agent: Agent, *args, **kwargs) -> bool:
+    @staticmethod
+    def redistributeFull(agent: Agent, investor: Agent, *args, **kwargs) -> bool:
         """ Redistribute earnings AND investment(s) to a specific investor
 
-            :param agent: the agent to redistribute to
+            :param agent: the agent on which the behavior operates
             :type agent: Agent
+            :param investor: the investor to redistribute to
+            :type investor: Agent
             :returns: wether the redistribution was successfull or not
             :rtype: bool
         """
         raise NotImplementedError
-
-    def redistributeAll(self):
-        """ Redistribute earnings to all investors """
-        for investor in self._investors:
-            self.redistribute(investor)
-
-    def redistributeFullAll(self):
-        """ Redistribute earnings AND investments(s) to all investors"""
-        for investor in self._investors:
-            self.redistributeFull(investor)
