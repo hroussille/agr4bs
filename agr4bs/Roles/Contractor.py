cadef50e913dabddaff6137652079d3cd3650b1e
Hector Roussille <hector.roussille@gmail.com>
2022-05-06 12:04:29 +0200

    Refactored Contractor

--- agr4bs/Roles/Contractor.py
+++ agr4bs/Roles/Contractor.py
@@ -1,7 +1,18 @@
+from ..Agent import StateChange
 from ..Role import Role, RoleType
+
+
+class ContractorStateChange(StateChange):
+
+    def __init__(self) -> None:
+        super().__init__()
 
 
 class Contractor(Role):
 
     def __init__(self) -> None:
         super().__init__(RoleType.CONTRACTOR)
+
+    @staticmethod
+    def stateChange() -> StateChange:
+        return ContractorStateChange()
