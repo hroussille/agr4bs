a1b3c28d8d95384d26cf3cc076afacdbeecf8b8a
Hector Roussille <hector.roussille@gmail.com>
2022-05-06 12:04:42 +0200

    Refactored GroupManager

--- agr4bs/Roles/GroupManager.py
+++ agr4bs/Roles/GroupManager.py
@@ -1,5 +1,13 @@
 from ..Role import Role, RoleType
-from ..Agent import Agent
+from ..Agent import Agent, StateChange
+
+
+class GroupManagerStateChange(StateChange):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.groupEnteringPolicy = None
 
 
 class GroupManager(Role):
@@ -7,10 +15,17 @@ class GroupManager(Role):
     def __init__(self) -> None:
         super().__init__(RoleType.GROUP_MANAGER)
 
-    def authorize(self, agent: Agent, *args, **kwargs) -> bool:
+    @staticmethod
+    def stateChange() -> StateChange:
+        return GroupManagerStateChange()
+
+    @staticmethod
+    def authorize(agent: Agent, candidate: Agent, *args, **kwargs) -> bool:
         """ authorizes an agent to enter the group or not
 
-            :param agent: the agent to authorize
+            :param agent: the agent on which the behavior operates
+            :type agent: Agent
+            :param candidate: the agent to authorize
             :type agent: Agent
             :returns: wether the agent was authorized or not
             :rtype: bool
