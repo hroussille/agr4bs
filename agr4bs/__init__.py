7e0312b0ba829c195ad6cbb4f0e554403b9d32a0
Hector Roussille <hector.roussille@gmail.com>
2022-05-10 16:34:57 +0200

    Reorder agr4bs/__init__.py to avoid circular import

--- agr4bs/__init__.py
+++ agr4bs/__init__.py
@@ -2,10 +2,6 @@
     Main agr4bs module
 """
 
-from . import groups
-from . import roles
-from . import common
-
 from .agent import Agent
 from .group import Group, GroupType
 from .role import Role, RoleType
@@ -14,3 +10,7 @@ from .common.transaction import Transaction
 from .common.investment import Investment
 from .common.payload import Payload
 from .common.blockchain import Blockchain
+
+from . import groups
+from . import roles
+from . import common
