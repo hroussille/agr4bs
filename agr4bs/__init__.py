1e0a4b18dc3d341923cccfd38a994ff5671b56a6
Hector Roussille <hector.roussille@gmail.com>
2022-05-11 10:20:07 +0200

    Exported Environment class and environment sub-module from main agr4bs module

--- agr4bs/__init__.py
+++ agr4bs/__init__.py
@@ -10,7 +10,9 @@ from .common.transaction import Transaction
 from .common.investment import Investment
 from .common.payload import Payload
 from .common.blockchain import Blockchain
+from .environment.environment import Environment
 
 from . import groups
 from . import roles
 from . import common
+from . import environment
