a154a2b711c40ee4abab4d0c479c488265cde658
Hector Roussille <hector.roussille@gmail.com>
2022-05-22 20:44:22 +0200

    Added core event names

---
+++ agr4bs/events/__init__.py
@@ -0,0 +1,26 @@
+
+
+from .events import REQUEST_INBOUND_PEER
+from .events import ACCEPT_INBOUND_PEER
+from .events import DENY_INBOUND_PEER
+from .events import DROP_INBOUND_PEER
+from .events import REQUEST_OUTBOUND_PEER
+from .events import ACCEPT_OUTBOUND_PEER
+from .events import DENY_OUTBOUND_PEER
+from .events import DROP_OUTBOUND_PEER
+from .events import PROPOSE_BLOCK
+from .events import REQUEST_BLOCK
+from .events import REQUEST_BLOCK_ENDORSEMENT
+from .events import ACCEPT_BLOCK_ENDORSEMENT
+from .events import DENY_BLOCK_ENDORSEMENT
+from .events import DIFFUSE_TRANSACTION
+from .events import REQUEST_TRANSACTION_ENDORSEMENT
+from .events import ACCEPT_TRANSACTION_ENDORSEMENT
+from .events import DENY_TRANSACTION_ENDORSEMENT
+from .events import PAUSE_SIMULATION
+from .events import RESTART_SIMULATION
+from .events import STOP_SIMULATION
+from .events import INIT
+from .events import BOOTSTRAP_PEERS
+from .events import UPDATE_PEERS
+from .events import REQUEST_PEER_DISCOVERY
---
+++ agr4bs/events/events.py
@@ -0,0 +1,43 @@
+
+# Seed nodes events
+BOOTSTRAP_PEERS = "request_seed_list"
+
+# P2P update events
+UPDATE_PEERS = "update_peers"
+REQUEST_PEER_DISCOVERY = "request_peer_discovery"
+
+# Inbound peers events
+REQUEST_INBOUND_PEER = "request_inbound_peer"
+ACCEPT_INBOUND_PEER = "accept_inbound_peer"
+DENY_INBOUND_PEER = "deny_inbound_peer"
+DROP_INBOUND_PEER = "drop_inbound_peer"
+
+# Outbound peers events
+REQUEST_OUTBOUND_PEER = "request_outbound_peer"
+ACCEPT_OUTBOUND_PEER = "accept_outbound_peer"
+DENY_OUTBOUND_PEER = "deny_outbound_peer"
+DROP_OUTBOUND_PEER = "drop_outbound_peer"
+
+# Block events
+PROPOSE_BLOCK = "propose_block"
+REQUEST_BLOCK = "request_block"
+REQUEST_BLOCKS = "request_blocks"
+
+# Block endorsement events
+REQUEST_BLOCK_ENDORSEMENT = "request_block_endorsement"
+ACCEPT_BLOCK_ENDORSEMENT = "accept_block_endorsement"
+DENY_BLOCK_ENDORSEMENT = "deny_block_endorsement"
+
+# Transaction diffusion events
+DIFFUSE_TRANSACTION = "diffuse_transaction"
+
+# Transaction endorsement events
+REQUEST_TRANSACTION_ENDORSEMENT = "request_transaction_endorsement"
+ACCEPT_TRANSACTION_ENDORSEMENT = "accept_transaction_endorsement"
+DENY_TRANSACTION_ENDORSEMENT = "deny_transaction_endorsement"
+
+# Simulation events
+INIT = "init"
+PAUSE_SIMULATION = "pause_simulation"
+RESTART_SIMULATION = "restart_simulation"
+STOP_SIMULATION = "stop_simulation"
