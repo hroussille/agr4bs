0d54f3b3d0c2fff3de399197ceede836ccf90b15
Hector Roussille <hector.roussille@gmail.com>
2022-06-14 18:28:40 +0200

    Lowered default factory network packet drop to 1%

--- agr4bs/factory/default_factory.py
+++ agr4bs/factory/default_factory.py
@@ -71,6 +71,6 @@ class Factory:
             Builds a black box Network implementation
         """
         if Factory.__network is None or reset is True:
-            Factory.__network = AioNetwork(delay=0.060, drop_rate=0.1)
+            Factory.__network = AioNetwork(delay=0.060, drop_rate=0.01)
 
         return Factory.__network
