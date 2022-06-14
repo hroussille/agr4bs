98d0f381fe210b7eb28b3ec3936f85abe9f9d3fe
Hector Roussille <hector.roussille@gmail.com>
2022-06-14 18:33:08 +0200

    Added temporary election strategies to block_creator_elector role - Fixed strategy (1st agent) - Random strategy (1 agent at random) - Fork inducing strategy (n agents at random)

--- agr4bs/roles/block_creator_elector.py
+++ agr4bs/roles/block_creator_elector.py
@@ -45,5 +45,8 @@ class BlockCreatorElector(Role):
             This is a system event simulating a normal election process
             such as PoW or PoS potentially leading to a block proposal.
         """
-        selected = random.choice(agent.agents_names)
+        #selected = random.choice(agent.agents_names)
+        selected = agent.agents_names[0]
+        #selected = random.sample(agent.agents_names, 2)
+        #selected = random.sample(agent.agents_names, random.randint(1, 2))
         await agent.send_system_message(CreateBlock(agent.name), selected)
