8fc412da4ee01729486460067d12a83365adaae5
Hector Roussille <hector.roussille@gmail.com>
2022-06-14 18:34:13 +0200

    Sanitized test_block_creation.py - This test currently fails most of the time : dynamic peers must be able to request missing blocks to each other.

--- tests/environment/test_block_creation.py
+++ tests/environment/test_block_creation.py
@@ -2,7 +2,10 @@
     Test suite for the Environment class related to agents addition
 """
 
+import random
 import asyncio
+import numpy as np
+from collections import Counter
 import agr4bs
 
 
@@ -11,12 +14,14 @@ async def test_block_creation():
         Test that the environment can be run and stopped later
         with no error or unwaited tasks
     """
+    random.seed(1)
+
     agr4bs.Factory.build_network(reset=True)
     genesis = agr4bs.Block(None, "genesis", [])
 
     agents = []
 
-    for i in range(100):
+    for i in range(50):
         agent = agr4bs.ExternalAgent(f"agent_{i}", genesis)
         agent.add_role(agr4bs.roles.Peer())
         agent.add_role(agr4bs.roles.BlockchainMaintainer())
@@ -32,12 +37,31 @@ async def test_block_creation():
 
     env_task = asyncio.create_task(env.run())
 
-    await asyncio.sleep(10)
+    await asyncio.sleep(5)
+
+    env.remove_role(agr4bs.roles.BlockCreatorElector())
+
+    await asyncio.sleep(1)
 
     await env.stop()
     await env_task
 
-    for agent in agents:
-        print(agent.name + " Blockchain height : " +
-              str(agent.context['blockchain'].head.height))
-        print(agent.context['state'].get_account_balance("agent_1"))
+    # heights = np.array(
+    #    [agent.context['blockchain'].head.height for agent in agents])
+    #avg_height = np.average(heights)
+    #std_height = np.std(heights)
+
+    head_hashes = [agent.context['blockchain'].head.hash for agent in agents]
+    head_counts = Counter(head_hashes)
+
+    # Ensure that one head is shared by all agents
+    # i.e., state is consensual
+    for _, head_count in head_counts.items():
+        assert head_count / len(agents) == 1
+
+    #print("Average blockchain height : " + str(avg_height))
+    #print("Std dev height : " + str(std_height))
+
+    # for head_hash, head_count in head_counts.items():
+    #    print("Head : " + head_hash + " : " +
+    #          str(100 * head_count / len(agents)) + " % ")
